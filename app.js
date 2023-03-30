d24d3e52f9820278d1e7e762058537f6570aaa9a
SUMON <SUMON.MATHJNU@GMAIL.COM>
2023-03-30 17:57:00 +0600

    sending file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const button=document.querySelector('#search-button');
 const city=document.querySelector('#city');
 const temperature=document.querySelector('#temperature');
 const lead=document.querySelector('.lead');
+const img=document.querySelector('#img');
 
 button.addEventListener('click',function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=84883e11040da6574183ab755f41fb1f')
@@ -11,10 +12,11 @@ button.addEventListener('click',function(){
     const cityName=data.name;
     document.querySelector('#city').innerText=cityName;
     document.querySelector('#input').value='';
+    // document.querySelector('#img').innerText=data.weather[0].icon;
     const temp=data.main.temp;
     const newTemp=temp- 273.15;
-    document.querySelector('#temperature').innerText=newTemp;
-    const lead=data.weather[0].main;
+    document.querySelector('#temperature').innerText=Math.round(newTemp);
+    const lead=data.weather[0].description;
     document.querySelector('.lead').innerText=lead;
 })
 })
